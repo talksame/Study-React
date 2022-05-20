08879cb2a36803867fef4301992e7fbd7dfb3921
talksame <rnjsdndud4@gmail.com>
2022-05-20 23:42:07 +0900

    0520

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! I'm Kundal. <br></br>
+          Now, I start to Study React with AI.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/talksame"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I Want Study React
+          Github
         </a>
       </header>
     </div>
