51fcc6f3c8db7c2037ca99a798ee89dffe268384
talksame <rnjsdndud4@gmail.com>
2022-06-21 23:06:54 +0900

    0621

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*component
+  prop는 속성이다.*/
+function Header(props){
+  return <header>
+  <h1><a href='/' onClick={(event)=>{
+    event.preventDefault();
+    /*props는 컨포넌트의 모든 속성들을 가지고 있다. */
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
+</header>
+}
+
+function Nav(props){
+  const lis = []
+
+  for ( let i = 0 ; i < props.topics.length; i ++){
+    let t = props.topics[i]
+    lis.push(<li key = {t.id}>
+      <a 
+      id = {t.id} 
+      href={'/read/' + t.id}
+      onClick ={(event)=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a></li>)
+  }
+
+  return      <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>
+}
+
+function Article(){
+  return       <article>
+  <h2>Welcome</h2>
+  HELLO, WEB
+</article>
+}
+
+function App() { 
+  const topics = [
+    {id:1, title: 'html', body:'html is ...'},
+    {id:2, title: 'css', body:'css is ...'},
+    {id:3, title: 'js', body:'js is ...'},
+    
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! I'm Kundal. <br></br>
-          Now, I start to Study React with AI.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/talksame"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-      </header>
+    <div>
+      
+      <Header title = "REACT" onChangeMode = {()=>{
+        alert("HEADER");
+      }}></Header>
+      <Nav topics = {topics} onChangeMode = { (id)=>{
+        alert(id);
+      }}></Nav>
+      <Article></Article>
     </div>
   );
 }
