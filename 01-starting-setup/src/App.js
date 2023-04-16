008160835b2dc91a2f40203efd229be1cf9a84a5
unknown <rnjsdndud4@gmail.com>
2023-04-16 23:03:38 +0900

    0416

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -3,22 +3,36 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expense = [
     {
-      id : 'e1',
-      title : 'Car',
-      amount : 123.32, 
-      date: new Date(2023, 4, 12)},
+      id: 'e1',
+      title: 'Car',
+      amount: 123.32,
+      date: new Date(2023, 4, 12)
+    },
     {
-      id : 'e2',
-      title : 'Food',
-      amount : 321.32, 
-      date: new Date(2023, 4, 11)},
+      id: 'e2',
+      title: 'Food',
+      amount: 321.32,
+      date: new Date(2023, 4, 11)
+    },
   ];
 
   return (
     <div>
-      <ExpenseItem title = {expense[0].title} amount = {expense[0].amount} date = {expense[0].date} ></ExpenseItem>
-      <ExpenseItem title = {expense[1].title} amount = {expense[1].amount} date = {expense[1].date} ></ExpenseItem>
-    
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date} >
+      </ExpenseItem>
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 
 
 function ExpenseItem(props) {
-    let expenseDate =  new Date(2023, 4, 12);
-    let expenseTitle = 'Car';
-    let expenseAmount = 123.32;
-
+    //Event 호버, selection, click, 마우스가 어디 위치하는 것
+    // CSS 보드판 만들고, 보드칸, 연결, 말이
     return (
         <div className="expense-item">
+            <ExpenseDate date ={props.date}/>
             <div>{props.date.toISOString()}</div>
             <div className=".expense-item__description">
                 <h2>{props.title}</h2>
